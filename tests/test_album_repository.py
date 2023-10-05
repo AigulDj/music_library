1d435937f22667964e92c6dae6c07f9a2ddb5725
Aigul <aigul.djon@gmail.com>
2023-10-05 18:35:20 +0100

    Add #create and delete# methods

--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -36,3 +36,54 @@ def test_get_the_record_with_given_id(db_connection):
     album_repo = AlbumRepository(db_connection)
     album = album_repo.find(2)
     assert album == Album(2, 'Surfer Rosa', 1988, 1)
+
+"""
+When we call #create
+We get a new record in the database
+"""
+def test_create_new_record(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    album_repo = AlbumRepository(db_connection)
+    album = Album(None, "New Album Title", 2023, 1)
+    album_repo.create(album)
+    result = album_repo.all()
+
+    assert result == [
+        Album(1, 'Doolittle', 1989, 1),
+        Album(2, 'Surfer Rosa', 1988, 1),
+        Album(3, 'Waterloo', 1974, 2),
+        Album(4, 'Super Trouper', 1980, 2),
+        Album(5, 'Bossanova', 1990, 1),
+        Album(6, 'Lover', 2019, 3),
+        Album(7, 'Folklore', 2020, 3),
+        Album(8, 'I Put a Spell on You', 1965, 4),
+        Album(9, 'Baltimore', 1978, 4),
+        Album(10, 'Here Comes the Sun', 1971, 4),
+        Album(11, 'Fodder on My Wings', 1982, 4),
+        Album(12, 'Ring Ring', 1973, 2),
+        Album(13, "New Album Title", 2023, 1)
+    ]
+
+"""
+When we call #delete
+We remove a record from the database
+"""
+def test_delete_record(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    album_repo = AlbumRepository(db_connection)
+    album_repo.delete(2)
+    result = album_repo.all()
+
+    assert result == [
+        Album(1, 'Doolittle', 1989, 1),
+        Album(3, 'Waterloo', 1974, 2),
+        Album(4, 'Super Trouper', 1980, 2),
+        Album(5, 'Bossanova', 1990, 1),
+        Album(6, 'Lover', 2019, 3),
+        Album(7, 'Folklore', 2020, 3),
+        Album(8, 'I Put a Spell on You', 1965, 4),
+        Album(9, 'Baltimore', 1978, 4),
+        Album(10, 'Here Comes the Sun', 1971, 4),
+        Album(11, 'Fodder on My Wings', 1982, 4),
+        Album(12, 'Ring Ring', 1973, 2),
+    ]
